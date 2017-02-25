5f801e703de0164f69c0e059f8532f7366f620a1
mertkanyener <mertkanyener@outlook.com>
2017-02-25 19:48:42 +0300

    Decoding webpages

--- DecodingWeb.py
+++ DecodingWeb.py
@@ -4,4 +4,20 @@ from bs4 import BeautifulSoup
 #loading content from the desired URL
 base_url = 'http://www.nytimes.com'
 r = requests.get(base_url)
+
+#Decoding webpage
 soup = BeautifulSoup(r.text)
+
+#find and loop through all elements on the page with the
+#class name "story-heading"
+for i in soup.find_all(class_="story-heading"):
+    if i.a:
+        print(i.a.text.replace("\n", " ").strip())
+    else:
+        print(i.contents[0].strip())
+
+
+
+
+
+
