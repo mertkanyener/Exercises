68af9c90224c9042e8d9be957cbcbca21a6c9743
mertkanyener <mertkanyener@outlook.com>
2017-02-27 21:54:02 +0300

    Read from a file -- Completed

--- read_file.py
+++ read_file.py
@@ -15,5 +15,21 @@ def print_dict(d):
     for key in all_keys:
         print(key + ": " + str(d[key]))
 
-print_dict(read('nameslist.txt'))
+#print_dict(read('nameslist.txt'))
 
+
+def read_sun(file):
+    sun = {}
+    with open(file, 'r') as open_file:
+        lines = open_file.read().splitlines()
+        for line in lines:
+            key = line.split('/')[2]
+            if key in sun:
+                sun[key] += 1
+            else:
+                sun[key] = 1
+    return sun
+
+
+d = read_sun("Training_01.txt")
+print_dict(d)
