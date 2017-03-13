a1807770d2616b91c0ac5e1e20e3fefeeb40a4fb
mertkanyener <mertkanyener@outlook.com>
2017-03-13 21:18:40 +0300

    Guessing game two -- in progress

--- Ex2.py
+++ Ex2.py
@@ -1,30 +1,32 @@
-def oddEvenCheck():
+def odd_even_check():
     number = int(input("Please enter a number"))
-    if number%2 == 0:
-        print(str(number) +" is an even number.")
+    if number % 2 == 0:
+        print(str(number) + " is an even number.")
         if number%4 == 0:
             print(" and it is divisible by four.")
     else:
         print(str(number) + " is an odd number.")
 
 
-def divisibleCheck():
+def divisible_check():
     num = int(input("Please enter the number to be divided"))
     check = int(input("Please enter the check number"))
-    if num%check == 0:
+    if num % check == 0:
         result = num/check
         print(str(num) + " is divisible by " + str(check) + ".")
         print("Result: " + str(result))
     else:
         print(str(num) + " is NOT divisible by " + str(check) + ".")
 
+
 def main():
     choice = input("Please enter '1' for odd-even check, '2' for divisible check")
     if choice == "1":
-        oddEvenCheck()
+        odd_even_check()
     elif choice == "2":
-        divisibleCheck()
+        divisible_check()
     else:
         print("Invalid choice. System exit")
+
 
 main()
